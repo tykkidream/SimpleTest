c1e35bb845c853bb04b5f6cf06653d1bdaf4f9d6
tykkidream <tykkidream@gmail.com>
2017-03-25 17:06:17 +0800

    删除一些无用的类

--- src/main/java/com/pzj/framework/armyant/load/core/LoadData.java
+++
@@ -1,7 +0,0 @@
-package com.pzj.framework.armyant.load.core;
-
-/**
- * Created by Saber on 2017/3/18.
- */
-public interface LoadData {
-}
--- src/main/java/com/pzj/framework/armyant/load/core/datas/ArrrayData.java
+++
@@ -1,7 +0,0 @@
-package com.pzj.framework.armyant.load.core.datas;
-
-/**
- * Created by Saber on 2017/3/18.
- */
-public class ArrrayData {
-}
